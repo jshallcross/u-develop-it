62f5e45882eb9a90fcca44dfc08d2115182bb500
Jon Shallcross <jshallcross1@icloud.com>
2021-02-23 16:15:31 -0500

    Join feature

--- server.js
+++ server.js
@@ -30,7 +30,11 @@ db.all(`SELECT * FROM candidates`, (err, rows) => {
 */
 // Get all candidates
 app.get('/api/candidates', (req, res) => {
-    const sql = `SELECT * FROM candidates`;
+    const sql = `SELECT candidates.*, parties.name 
+            AS party_name 
+            FROM candidates 
+            LEFT JOIN parties 
+            ON candidates.party_id = parties.id`;
     const params = [];
     db.all(sql, params, (err, rows) => {
         if (err) {
@@ -55,8 +59,12 @@ db.get(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
 */
 // Get single candidate
 app.get('/api/candidate/:id', (req, res) => {
-    const sql = `SELECT * FROM candidates 
-                WHERE id = ?`;
+    const sql = `SELECT candidates.*, parties.name 
+            AS party_name 
+            FROM candidates 
+            LEFT JOIN parties 
+            ON candidates.party_id = parties.id 
+            WHERE candidates.id = ?`;
     const params = [req.params.id];
     db.get(sql, params, (err, row) => {
         if (err) {

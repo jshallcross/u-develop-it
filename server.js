85bec0e9758850220caa479a38c8f9db5556719c
Jon Shallcross <jshallcross1@icloud.com>
2021-02-23 14:04:56 -0500

    Post route added

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 const sqlite3 = require('sqlite3').verbose();
+const inputCheck = require('./utils/inputCheck');
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
@@ -109,6 +110,31 @@ db.run(sql, params, function(err, result) {
     console.log(result, this.lastID);
 });
 */
+// Create a candidate
+app.post('/api/candidate', ({ body }, res) => {
+    const errors = inputCheck(body, 'first_name', 'last_name', 'industry_connected');
+    if (errors) {
+        res.status(400).json({ error: errors });
+    return;
+    }
+    const sql = `INSERT INTO candidates (first_name, last_name, industry_connected) 
+            VALUES (?,?,?)`;
+    const params = [body.first_name, body.last_name, body.industry_connected];
+    // ES5 function, not arrow function, to use `this`
+    db.run(sql, params, function(err, result) {
+        if (err) {
+            res.status(400).json({ error: err.message });
+        return;
+        }
+
+    res.json({
+        message: 'success',
+        data: body,
+        id: this.lastID
+        });
+    });
+});
+
 // Default response for any other request(Not Found) Catch all
 app.use((req, res) => {
     res.status(404).end();
